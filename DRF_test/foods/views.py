f4472d27e7320b638964ef6b5420b5c8e20135c3
lisvv <81390205+lisvv@users.noreply.github.com>
2022-03-17 19:36:22 +0300

    add additional

--- DRF_test/foods/views.py
+++ DRF_test/foods/views.py
@@ -6,10 +6,6 @@ from django.db.models import Prefetch
 
 class FoodListApiView(generics.ListAPIView):
     serializer_class = FoodListSerializer
-    queryset = FoodCategory.objects.all().prefetch_related(
-        Prefetch(
-            "foods",
-            queryset=Food.objects.filter(is_publish=True)))
 
     def get_queryset(self):
         foods = Food.objects.filter(is_publish=True)
